cf9dc26e1c700fef5bdae1ef51fa136a6afe0862
Avnish Anand <62175110+anand-avnish@users.noreply.github.com>
2023-01-06 20:43:12 +0530

    Added color picker

--- src/script.js
+++ src/script.js
@@ -1,3 +1,22 @@
+//Carousel
+let name_bg = "#222831"
+let movie_bg="#00ADB5"
+let series_bg="#393E46"
+let pass_time_bg ="#EEEEEE"
+
+if(window.localStorage.getItem("name-color")==null)
+    addValueToLocalStorage("name-color",name_bg)
+if(window.localStorage.getItem("movie-color")==null)
+    addValueToLocalStorage("movie-color",movie_bg)
+if(window.localStorage.getItem("series-color")==null)
+    addValueToLocalStorage("series-color",series_bg)
+if(window.localStorage.getItem("pass-time-color")==null)
+    addValueToLocalStorage("pass_time-color",pass_time_bg)
+const series_color_input=document.getElementById("series-color-input")
+const name_color_input=document.getElementById("name-color-input")
+const movies_color_input=document.getElementById("movie-color-input")
+const pass_time_color_input=document.getElementById("pass-time-color-input")
+
 const delay = 12000;
 
 const slides = document.querySelector(".slides");
@@ -44,10 +63,13 @@ document.querySelector(".previous").addEventListener("click", function() {
     restart();
 });
 
+//Slide text input and update
+
 const change_name = document.querySelector('.change-name')
 const change_movie = document.querySelector('.change-movie')
 const change_series = document.querySelector('.change-series')
 const change_pass_time = document.querySelector('.change-pass_time')
+
 
 change_name.addEventListener("click",function(){
     const name = document.querySelector('.name')
@@ -110,4 +132,51 @@ function updateValueFromLocalStorage(){
         document.querySelector(".series").innerHTML=series_value
     if(pass_time_value&&pass_time_value!="")
         document.querySelector(".pass_time").innerHTML=pass_time_value
+
+    name_bg= getValueFromLocalStorage("name-color")
+    movie_bg= getValueFromLocalStorage("movie-color")
+    series_bg= getValueFromLocalStorage("series-color")
+    pass_time_bg= getValueFromLocalStorage("pass_time-color")
+    document.querySelector('.slide-name').style.backgroundColor=name_bg
+    document.querySelector('.slide-movie').style.backgroundColor=movie_bg
+    document.querySelector('.slide-series').style.backgroundColor=series_bg
+    document.querySelector('.slide-pass-time').style.backgroundColor=pass_time_bg
+    name_color_input.value=name_bg
+    movies_color_input.value=movie_bg
+    series_color_input.value=series_bg
+    pass_time_color_input.value=pass_time_bg
 }
+
+//Color picker
+
+name_color_input.addEventListener("change", function(){
+    // console.log(name_color_input.value);
+    const new_color=name_color_input.value
+    document.querySelector('.slide-name').style.backgroundColor=new_color
+    name_bg=new_color;
+    addValueToLocalStorage("name-color",name_bg)
+})
+
+movies_color_input.addEventListener("change", function(){
+    // console.log(name_color_input.value);
+    const new_color=movies_color_input.value
+    document.querySelector('.slide-movie').style.backgroundColor=new_color
+    movie_bg=new_color;
+    addValueToLocalStorage("movie-color",movie_bg)
+})
+
+series_color_input.addEventListener("change", function(){
+    // console.log(name_color_input.value);
+    const new_color=series_color_input.value
+    document.querySelector('.slide-series').style.backgroundColor=new_color
+    series_bg=new_color;
+    addValueToLocalStorage("series-color",series_bg)
+})
+
+pass_time_color_input.addEventListener("change", function(){
+    // console.log(name_color_input.value);
+    const new_color=pass_time_color_input.value
+    document.querySelector('.slide-pass-time').style.backgroundColor=new_color
+    pass_time_bg=new_color;
+    addValueToLocalStorage("pass-time-color",pass_time_bg)
+})
